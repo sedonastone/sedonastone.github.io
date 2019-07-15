b92ab74ef0fde3eae54bbecfd5de530de86b70c6
jontejada <hi@jontejada.com>
2019-07-15 11:38:49 -0700

    added photoswipe lib

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import 'react-photoswipe/lib/photoswipe.css';
+import { PhotoSwipeGallery, PhotoSwipe } from 'react-photoswipe';
+
+let items = [
+  {
+    src: 'http://lorempixel.com/1200/900/sports/1',
+    thumbnail: 'http://lorempixel.com/120/90/sports/1',
+    w: 1200,
+    h: 900,
+    title: 'Image 1'
+  },
+  {
+    src: 'http://lorempixel.com/1200/900/sports/2',
+    thumbnail: 'http://lorempixel.com/120/90/sports/2',
+    w: 1200,
+    h: 900,
+    title: 'Image 2'
+  }
+];
+
+let options = {
+  //http://photoswipe.com/documentation/options.html
+};
+
+const getThumbnailContent = (item) => {
+  return (
+    <img src={item.thumbnail} width={120} height={90}/>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
+        <p>hello world.</p>
+        <PhotoSwipeGallery items={items} options={options} thumbnailContent={getThumbnailContent}/>
+
       </header>
     </div>
   );
