61a76b824235254470bab2910362a4a3ed15599e
jontejada <hi@jontejada.com>
2019-07-17 16:18:04 -0700

    Updates

---
+++ precache-manifest.d1f4dabe6ac323b5e91177dd8d9618ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb746f2c3598e7ac20fdb2bcee833627",
+    "url": "/sedonasv/index.html"
+  },
+  {
+    "revision": "e61324b5063ba70736de",
+    "url": "/sedonasv/static/css/2.d4a6af3e.chunk.css"
+  },
+  {
+    "revision": "aaeb42ba6baa96163cb2",
+    "url": "/sedonasv/static/css/main.4225ebee.chunk.css"
+  },
+  {
+    "revision": "e61324b5063ba70736de",
+    "url": "/sedonasv/static/js/2.1b9b327e.chunk.js"
+  },
+  {
+    "revision": "aaeb42ba6baa96163cb2",
+    "url": "/sedonasv/static/js/main.81d27267.chunk.js"
+  },
+  {
+    "revision": "ec6dcfdc6e212ba67200",
+    "url": "/sedonasv/static/js/runtime~main.58e99b35.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/sedonasv/static/media/default-skin.b257fa9c.svg"
+  }
+]);
