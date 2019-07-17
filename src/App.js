61113c376c0b9d22a9b4e63dc16435863305a0dc
jontejada <hi@jontejada.com>
2019-07-17 16:11:53 -0700

    sections added

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ const styles = {
 };
 
 const getStyleNames = (names) => {
-  if (names.length === 1) {
+  if (!names) {
+    return '';
+  } else if (names.length === 1) {
     return styles[names[0]];
   } else {
     return 'Blend of ' + names.map((name) => styles[name]).join(' & ') + '.';
@@ -44,6 +46,10 @@ let options = {
   //http://photoswipe.com/documentation/options.html
   showHideOpacity: true,
   getThumbBoundsFn: false,
+  shareEl: false,
+  captionEl: false,
+  fullscreenEl: false,
+  zoomEl: false,
 };
 
 const getThumbnailContent = (item) => {
@@ -52,7 +58,62 @@ const getThumbnailContent = (item) => {
   );
 }
 
-const items = getPhotoGroup('gallery');
+const galleryItems = getPhotoGroup('gallery');
+const trimItems = getPhotoGroup('trim');
+
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      submitted: false,
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('https://docs.google.com/forms/d/e/1FAIpQLSfyUbgbMsnVviN_Feip4fBi22bhrqixjcXptkcZFH91ydYijA/formResponse', {
+      method: 'POST',
+      body: data,
+      mode: 'no-cors',
+    }).then((r) => {
+      console.log('hit');
+      this.setState({
+        submitted: true,
+      });
+    });
+  }
+
+  render() {
+    if (this.state.submitted) {
+      return (<div className="quote"><h1>Form sent. Thanks!</h1></div>)
+    }
+    return (
+      <form onSubmit={this.handleSubmit} className="quote">
+        {/* <div> */}
+          <label htmlFor="form_name">Name</label>
+          <input id="form_name" name="entry.153663014" type="text" />
+        {/* </div> */}
+        {/* <div> */}
+          <label htmlFor="form_email">Email</label>
+          <input id="form_email" name="entry.467162401" type="email" />
+        {/* </div> */}
+        {/* <div> */}
+          <label htmlFor="form_phone">Phone</label>
+          <input id="form_phone" name="entry.1630298825" type="tel" />
+        {/* </div> */}
+        {/* <div> */}
+          <label htmlFor="form_message">Message</label>
+          <textarea id="form_message" name="entry.738084605" rows="3" />
+        {/* </div> */}
+        <button>Send</button>
+      </form>
+    );
+  }
+}
+
 
 function App() {
   return (
@@ -63,9 +124,13 @@ function App() {
           <p>STONE</p>
           <p>VENEER</p>
         </div>
-        <div className="description">Family owned & operated local manufacturer of beautiful stone veneer. We are proud to offer our high quality products at factory direct prices.</div>
+        <div className="description">
+          <p>Local manufacturer of beautiful stone veneer.</p>
+          <p>High quality products at factory direct prices.</p>
+          <p>Family owned & operated. (602) 845 - 0004</p>
+        </div>
       </header>
-      <nav>
+      <nav id="navbar">
         <ul>
           <li><a href="#photos">Photos</a></li>
           <li><a href="#visit">Visit Us</a></li>
@@ -75,22 +140,52 @@ function App() {
       </nav>
       <main>
         <section id="photos" className="gallery">
-          <PhotoSwipeGallery items={items} options={options} thumbnailContent={getThumbnailContent}/>
+          {/* <p>A selection of photos from some of the many Sedona Stone Veneer installations in the Phoenix metro area and across the Southwest. These include remodels, new construction, small updates, homes, businesses, interiors, exteriors, and more. Click photos to enlarge.</p>  */}
+          <PhotoSwipeGallery items={galleryItems} options={options} thumbnailContent={getThumbnailContent}/>
         </section>
         <section id="visit">
-          <div>left</div>
-          <div>right</div>
+          <div className="questions">
+            <h2>(602) 845 – 0004</h2>
+            <i>Give us a call for a free quote & more info. Or use this form:</i>
+            <Form />
+          </div>
+          
+          <div className="map">
+            <h2>9605 East Main St. Mesa, AZ 85207</h2>
+            <i>At the SE corner of 96th St. & Main St. (Apache Trail), next to Craft City.</i>
+            <h3>‍Mon to Fri: 8 – 4:30</h3>
+            <h3>‍Sat: Please call ahead for hours</h3>
+            <iframe width="100%" height="450" frameBorder="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJgVBnsl6wK4cRNDqotuzkOhc&key=AIzaSyCVHr2ug300Slszr1DiJrTCjqvO2Kmddhg&zoom=10"></iframe>
+          </div>
         </section>
         <section id="stones">
-          10 grid in single image?
+          <h2>10 Stone Styles</h2>
+          <div className="stonecontainer">
+            {/* <div className="stonerow"> */}
+              <img className="sq" src="img/sq_qf.jpg" alt="Stone style: Quick Fit" />
+              <img className="sq" src="img/sq_as.jpg" alt="Stone style: Ashlarstone" />
+              <img className="sq" src="img/sq_fs.jpg" alt="Stone style: Fieldstone" />
+            {/* </div> */}
+            {/* <div className="stonerow"> */}
+              <img className="rc" src="img/rc_rb.jpg" alt="Stone style: Rustic Brick" />
+              <img className="rc" src="img/rc_cl.jpg" alt="Stone style: Chiseled Limestone" />
+              <img className="rc" src="img/rc_tl.jpg" alt="Stone style: Tumbled Ledgestone" />
+              <img className="rc" src="img/rc_rs.jpg" alt="Stone style: Riverstone" />
+            {/* </div> */}
+            {/* <div className="stonerow"> */}
+              <img className="sq" src="img/sq_sl.jpg" alt="Stone style: Split Ledgestone" />
+              <img className="sq" src="img/sq_cs.jpg" alt="Stone style: Castlestone" />
+              <img className="sq" src="img/sq_ss.jpg" alt="Stone style: Stackstone" />
+            {/* </div> */}
+          </div>
+          {/* <div id="spacer" /> */}
         </section>
         <section id="trim" className="gallery">
-          <PhotoSwipeGallery items={[items[0]]} options={options} thumbnailContent={getThumbnailContent}/>
+          <h2>Wide selection of trim</h2>
+          <PhotoSwipeGallery items={trimItems} options={options} thumbnailContent={getThumbnailContent}/>
         </section>
-
       </main>
       <footer>
-      f
       </footer>
     </>
   );
