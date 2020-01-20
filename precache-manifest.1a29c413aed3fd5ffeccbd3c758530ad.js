091f27b6a07706131c7fac69753b015ad7bca809
jontejada <hi@jontejada.com>
2020-01-19 21:29:28 -0800

    Updates

---
+++ precache-manifest.1a29c413aed3fd5ffeccbd3c758530ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4aafccc2222d813e8189462e5258a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77c6630746ca37ab3a5a",
+    "url": "/static/css/2.d45e8b06.chunk.css"
+  },
+  {
+    "revision": "afc0d1d1ead4ba70a3de",
+    "url": "/static/css/main.285ea470.chunk.css"
+  },
+  {
+    "revision": "77c6630746ca37ab3a5a",
+    "url": "/static/js/2.8c1ffe6a.chunk.js"
+  },
+  {
+    "revision": "3abf02ab9675c2910d733fa14dcea33b",
+    "url": "/static/js/2.8c1ffe6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afc0d1d1ead4ba70a3de",
+    "url": "/static/js/main.b5a14614.chunk.js"
+  },
+  {
+    "revision": "057e2005ff8f5181b02c",
+    "url": "/static/js/runtime-main.df81eddf.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbe64fc2ae185e08bb69bb1f248bd826.js"
+  "/precache-manifest.1a29c413aed3fd5ffeccbd3c758530ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
